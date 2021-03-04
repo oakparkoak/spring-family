6a1f28d1d85768f479f51b3acd7c1f2abac786fd
ElijahDu <withxlaser@gmail.com>
2021-03-04 13:55:55 +0800

    :truck: rename modules

---
+++ B-jdbc/B05-template/src/main/java/org/oakparkoak/JdbcTemplateApp.java
@@ -0,0 +1,14 @@
+package org.oakparkoak;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @package: org.oakparkoak
+ * @author: Captain
+ * @time: 2/8/2021 6:04 PM
+ */
+@Slf4j
+@SpringBootApplication
+public class JdbcTemplateApp {
+}
